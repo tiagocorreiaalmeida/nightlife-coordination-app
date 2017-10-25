7ee66ecaaaf8a00fbc63e455614884aeb298d596
tiagocorreia <tiagoalmeida.correia@gmail.com>
2017-10-25 23:27:53 +0100

    Routes Update/Fix

--- public/js/main.js
+++ public/js/main.js
@@ -4,15 +4,12 @@ $(document).ready((()=>{
         let value = $("#location").val();
         if(value!=="" && value !== latest){
             $.getJSON("/search/"+value,((data)=>{
-                console.log(data);
                 if(data){
                     $("#dataInfo").empty();
                     data.forEach((ele)=>{
-                        $("#dataInfo").append('<div class="col-md-12 card mb-4"><div class="row"><div class="col-sm-3"><img class="card-img-top" src="'+ele.img+'" alt="Card image"></div><div class="col-sm-9"><h4 class="mt-3 mb-3"><a href="'+ele.url+'">'+ele.name+'</a> - <i class="fa fa-tag" aria-hidden="true"></i> '+ele.type+'</h4><p><i class="fa fa-map-marker" aria-hidden="true"></i>'+ele.address+'</p><p class="card-text">Rating: '+ele.rating+'</p><div class="row"><div class="col-sm-4"><button id="go" class="btn btn-info btn-block" data-attr="'+ele.going.length+'" >Going: '+ele.going.length+'</button></div><div class="col-sm-4 col-6"> <a href="callto:'+ele.phone+'" class="btn btn-outline-success btn-block"><i class="fa fa-phone" aria-hidden="true"></i></a></div><div class="col-sm-4 col-6"><a href="http://maps.google.com/?q='+ele.coord+'" target="_blank" class="btn btn-outline-info btn-block"><i class="fa fa-globe" aria-hidden="true"></i></a></div></div></div></div></div>');
+                        $("#dataInfo").append('<div class="col-md-12 card mb-4"><div class="row"><div class="col-sm-3"><img class="card-img-top" src="'+ele.img+'" alt="Card image"></div><div class="col-sm-9"><h4 class="mt-3 mb-3"><a href="'+ele.url+'">'+ele.name+'</a> - <i class="fa fa-tag" aria-hidden="true"></i> '+ele.type+'</h4><p><i class="fa fa-map-marker" aria-hidden="true"></i>'+ele.address+'</p><p class="card-text">Rating: '+ele.rating+'</p><div class="row"><div class="col-sm-4"><button id="go" class="btn btn-info btn-block" data-attr="'+ele.id_place+'" >Going: '+ele.users_going.length+'</button></div><div class="col-sm-4 col-6"> <a href="callto:'+ele.phone+'" class="btn btn-outline-success btn-block"><i class="fa fa-phone" aria-hidden="true"></i></a></div><div class="col-sm-4 col-6"><a href="http://maps.google.com/?q='+ele.coord+'" target="_blank" class="btn btn-outline-info btn-block"><i class="fa fa-globe" aria-hidden="true"></i></a></div></div></div></div></div>');
                     });
-                }else{
-                    console.log("no data found");
-                }       
+                }     
             }))
             .fail((e)=>{
                 console.log(e);
@@ -24,10 +21,9 @@ $(document).ready((()=>{
         let element = $(this);
         let buttonid = $(this).attr("data-attr");
         $.getJSON("/go/"+buttonid,((data)=>{
-            if(data){
-                element.text("Going: " + data["size"]);
+            if(data && !data.error){
+                element.text("Going: " + data["users_going"]);
             }
         }));
     });
 }));
-    //<button id="go" class="btn btn-info btn-block" data-attr="'+ele.id+'" >Going: '+ele.going+'</button>
--- routes/routes.js
+++ routes/routes.js
@@ -7,91 +7,66 @@ const moment = require("moment");
 const Place = require("../models/places");
 const User = require("../models/user");
 
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
     res.render("index");
 });
 
-router.get("/search/:location",(req,res)=>{
-    //save requet that has people going and for each city check if is there any people going with going set on the db
-   let userLocation = req.params.location;
-   yelp.accessToken(process.env.YELP_ID,process.env.YELP_SECRET).then((response)=>{
+router.get("/search/:location", (req, res) => {
+    let userLocation = req.params.location;
+    let clientRequest = yelp.accessToken(process.env.YELP_ID, process.env.YELP_SECRET).then((response) => {
         const client = yelp.client(response.jsonBody.access_token);
-        client.search({
-            term:"bar",
-            location:userLocation,
-            open_now:true,
-            limit:1
-        }).then((resData)=>{
-            let responseData = resData.jsonBody.businesses.map((ele)=>{
-                    Place.findOne({"id_place":ele.id}).then((data)=>{
-                        if(data){
-                            return data.toObject();
-                        }else{
-                            let place = new Place({
-                                id_place: ele.id,
-                                name: ele.name,
-                                type: ele.categories[0].title,
-                                img: ele.image_url,
-                                url: ele.url,
-                                rating: ele.rating,
-                                phone: ele.phone,
-                                coord: ele.coordinates.latitude +","+ele.coordinates.longitude,
-                                address: ele.location.display_address[0] + " " + ele.location.display_address[1] + " " + ele.location.display_address[2],
-                                users_going: []
-                            });
-                            place.save().then((placeData)=>{
-                                return placeData.toObject();
-                            }).catch((e)=>{
-                                console.log(e);
-                            });
-                        }
-                    }).catch((e)=>{
-
-                    });
-            });
-            res.send(JSON.stringify(responseData));
-        }).catch((e)=>{console.log(e);});
-    }).catch((e)=>{console.log(e);}); 
+        return client.search({
+            term: "bar",
+            location: userLocation,
+            open_now: true,
+            limit: 1
+        });
+    });
+    clientRequest.then((resData) => {
+        let retrievePlace = ele => Place.findOne({ id_place: ele.id }).then(data => {
+            if (data) return data.toObject();
+            return new Place({
+                id_place: ele.id,
+                name: ele.name,
+                type: ele.categories[0].title,
+                img: ele.image_url,
+                url: ele.url,
+                rating: ele.rating,
+                phone: ele.phone,
+                coord: ele.coordinates.latitude + "," + ele.coordinates.longitude,
+                address: ele.location.display_address[0] + " " + ele.location.display_address[1] + " " + ele.location.display_address[2],
+                users_going: []
+            })
+                .save()
+                .then(data => data.toObject());
+        });
+        Promise.all(resData.jsonBody.businesses.map(retrievePlace)).then((places) => {
+            res.send(JSON.stringify(places))
+        });
+    }).catch((e) => { console.log(e); });
 });
 
-router.get("/go/:id",(req,res)=>{
-    let location = req.params.id;
-
-    Place.findOne({id_place:location}).then((data)=>{
-        if(data){
-            let length = data.users_going.length;
-            Place.findOne({id_place:location,users_going:"anothernewuser"}).then((data)=>{
-                if(data){
-                    Place.update({id_place:location,users_going:"anothernewuser"},{$pull:{users_going:"anothernewuser"}}).then((data)=>{
-                            let dataUpdated ={size: length-=1};
-                            res.send(JSON.stringify(dataUpdated));
-                    }).catch((e)=>{
-                        console.log(e);
-                    });
-                }else{
-                    Place.update({id_place:location},{$push:{users_going:"anothernewuser"}}).then((data)=>{
-                        let dataUpdated ={size: length+=1};
-                        res.send(JSON.stringify(dataUpdated));
-                    }).catch((e)=>{
-                        console.log(e);
-                    })
-                }
-            }).catch((e)=>{
-                console.log(e);
-            })
-        }else{
-            Place.create({id_place:location,users_going:"anothernewuser"}).then((data)=>{
-                let dataUpdated = {size:1};
-                res.send(JSON.stringify(dataUpdated));
-            }).catch((e)=>{
-                console.log(e);
-            });
-
-        }
-    }).catch((e)=>{
-        console.log(e);
-    })
-
+router.get("/go/:id", (req, res) => {
+    if (req.user) {
+        let userID = req.user.id;
+        let location = req.params.id;
+        Place.findOne({ id_place: location }).then((data) => {
+            if (data) {
+                Place.findOne({ id_place: location, users_going: userID}).then((data) => {
+                    if (data) {
+                        return Place.findOneAndUpdate({ id_place: location, users_going: userID }, { $pull: { users_going: userID }},{new:true});
+                    } else {
+                        return Place.findOneAndUpdate({ id_place: location }, { $push: { users_going: userID }},{new:true});
+                    }
+                }).then((data)=>{
+                    let users_going = data.toObject().users_going.length;
+                    res.send(JSON.stringify({users_going}));
+                }).catch((e) => {
+                    console.log(e);
+                });
+            } else { res.send(JSON.stringify({ error: true })); }
+        }).catch((e) => console.log(e));
+    } else { res.send(JSON.stringify({ error: true })); }
 });
 
 module.exports = router;
